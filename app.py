11b13de77ac136d4117ecd10b49948ff84bae3d1
harikn77 <hariknair77@gmail.com>
2020-05-12 06:31:51 +0530

    login change

--- app.py
+++ app.py
@@ -32,27 +32,31 @@ while(date < end):
     
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-
 creds = None
-# The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
-        
-service = build('calendar', 'v3', credentials=creds)
+service = None
+
+def login():
+    global creds
+    global service
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+            
+    service = build('calendar', 'v3', credentials=creds)
 
 def addEvent(date = None):
     event = {
@@ -79,6 +83,8 @@ def createEvent():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global title
+    if not creds:
+        login()
     if request.method == 'POST':
         title = request.form['topic']
         start = time.time()
